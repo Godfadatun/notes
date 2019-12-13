c9cb34bfdc49336ba39cf7f44be1078e676fc551
Godfadatun <danyadegoke@gmail.com>
2019-12-13 09:19:11 -0500

    building controller

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-// app.use('/user', usersRouter);
+app.use('/user', usersRouter);
 
 
 // catch 404 and forward to error handler
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,55 @@
 var express = require('express');
 var router = express.Router();
 
+var tag = require('../models').tag
+var user = require('../models').user
+var media = require('../models').media
+var note = require('../models').note
+// console.log(tag);
+
+
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/user', function(req, res, next) {
+  // res.send('respond with a resource');
+  var myUser = user.findAll().then(users => {
+    console.log("All users:", JSON.stringify(users, null, 4));
+  });
+
+  res.json({
+    status: "success",
+    message: "success",
+    data: "success",
+    model: myUser
+  })
 });
+
+
+
+router.get('/note', function(req, res, next) {
+
+  var myNote = note.findAll().then(users => {
+    console.log("All notes:", JSON.stringify(users, null, 4));
+  });
+
+  res.json({
+    status: "success",
+    message: "success",
+    data: myNote
+  })
+});
+
+
+// Posts
+router.post('/user', function (req, res) {
+  // var myUser = user.create({ name: "Jane", email: "Doe" }).then(jane => {
+  //   console.log("Jane's auto-generated ID:", jane.id);
+  // });
+
+  // console.log(req);
+  
+  res.send('req')
+})
 
 
 module.exports = router;
