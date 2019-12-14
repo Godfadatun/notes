46bf51ccda0bbb67537f3f53bc8f9257984f22f3
Godfadatun <danyadegoke@gmail.com>
2019-12-14 01:16:21 -0500

    auth endpoint built

--- routes/users.js
+++ routes/users.js
@@ -10,46 +10,156 @@ var note = require('../models').note
 
 
 /* GET users listing. */
-router.get('/user', function(req, res, next) {
-  // res.send('respond with a resource');
-  var myUser = user.findAll().then(users => {
-    console.log("All users:", JSON.stringify(users, null, 4));
-  });
 
-  res.json({
-    status: "success",
-    message: "success",
-    data: "success",
-    model: myUser
+  // user Table
+router.route('/user')
+  .get(function (req, res) {
+    user.findAll().then(response => {
+        // console.log("All users:", JSON.stringify(users, null, 4));
+      res.json({
+        status: "success",
+        message: "success",
+        data: "success",
+        model: response
+      })
+    });
   })
-});
-
-
-
-router.get('/note', function(req, res, next) {
-
-  var myNote = note.findAll().then(users => {
-    console.log("All notes:", JSON.stringify(users, null, 4));
-  });
-
-  res.json({
-    status: "success",
-    message: "success",
-    data: myNote
+  .post(function (req, res) {
+    user.create({
+        name: req.body.name, 
+        email: req.body.email, 
+        password: req.body.password,
+        is_admin: false
+    })
+    .then(response => {
+        res.json({
+          status: "success",
+          message: "success",
+          data: response
+        })
+    });
   })
-});
 
 
-// Posts
-router.post('/user', function (req, res) {
-  // var myUser = user.create({ name: "Jane", email: "Doe" }).then(jane => {
-  //   console.log("Jane's auto-generated ID:", jane.id);
-  // });
+  router.post('/auth', function (req, res, next) {
+    console.log(req.body)
+    user.findOne({ 
+      where: {
+        email: req.body.email, 
+        password: req.body.password,
+      } 
+    })
+    .then(response => {
+      res.json({
+        status: "success",
+        message: "success",
+        data: response
+      })
+    });
+  })
 
-  // console.log(req);
-  
-  res.send('req')
+
+
+  // note Table
+router.route('/note')
+  .get(function (req, res, next) {
+    note.findAll().then(response => {
+      res.json({
+        status: "success",
+        message: "success",
+        data: response
+      })
+    });
+  })
+  .post(function (req, res, next) {
+    note.create({
+      user_id: 1234, 
+      body: req.body.text,
+      is_deleted_at: new Date(),
+   })
+   .then(response => {
+       res.json({
+         status: "success",
+         message: "success",
+         data: response
+       })
+   });
+ })
+
+
+
+  // media Table
+router.route('/media')
+  .get(function (req, res, next) {
+    media.findAll().then(response => {
+      res.json({
+        status: "success",
+        message: "success",
+        data: response
+      })
+    });
+  })
+  .post(function (req, res, next) {
+    media.create({
+      user_id: 1234, 
+      note_id: 5678,
+      media:'',
+      extension:'',
+    })
+    .then(response => {
+        res.json({
+          status: "success",
+          message: "success",
+          data: response
+        })
+    });
+  })
+
+
+  // tag Table
+router.route('/tag')
+  .get(function (req, res, next) {
+    tag.findAll().then(response => {
+      res.json({
+        status: "success",
+        message: "success",
+        data: response
+      })
+    });
+  })
+  .post(function (req, res, next) {
+    tag.create({
+      user_id: 1234, 
+      note_id: 5678,
+  })
+  .then(response => {
+      res.json({
+        status: "success",
+        message: "success",
+        data: response
+      })
+  });
 })
+
+
+
+
+
+
+
+
+
+// Delete ===========================
+router.put(function (req, res) {
+  user.destroy({
+    where: {
+      id: 1
+    }
+  }).then(() => {
+    console.log("Done");
+  });
+})
+
 
 
 module.exports = router;
